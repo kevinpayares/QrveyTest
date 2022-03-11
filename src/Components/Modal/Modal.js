77246be0744884e540a8dae3c6a564d398ce3bc8
Kevin Angel Payares Sepulveda <kevinpayares1@gmail.com>
2022-03-11 14:46:11 -0500

    Se agrega al componente modal la opcion de seleccionar pais como favorito

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -15,23 +15,50 @@ class Modal extends React.Component{
         this.addToFavorite = this.addToFavorite.bind(this);
     }
 
+    
     addToFavorite(){
-        if(this.state.isFavorite){
+        let nameCountry = this.props.countryDetail.name.common;
+        let index;
+        let listFavorite = JSON.parse(localStorage.getItem('listFavorite'));
+        
+        if(listFavorite != null && listFavorite.includes(nameCountry)){
+            
             this.setState({isFavorite: false})
+            index = listFavorite.indexOf(nameCountry);
+            listFavorite.splice(index);
+            localStorage.setItem('listFavorite', JSON.stringify(listFavorite));
+
         }else{
+            
             this.setState({isFavorite: true})
+
+            if(listFavorite == null){
+                listFavorite= [];
+            }
+
+            listFavorite.push(nameCountry);
+            localStorage.setItem('listFavorite', JSON.stringify(listFavorite));
+
         }
     }
 
     render(){
+
         if(this.props.showModal){
-            
+
+            let listFavorite = JSON.parse(localStorage.getItem('listFavorite'));
             let countryDetail = this.props.countryDetail
             let currencie = countryDetail.hasOwnProperty("currencies") ? Object.values(countryDetail.currencies)[0].name : "";
             let languages = countryDetail.hasOwnProperty("languages") ? Object.values(countryDetail.languages)[0] : "";
             let population = parseInt(countryDetail.population / 1000000);
             let listCountry = "";
             let colorStar = this.state.isFavorite ? "#FFD700" : "#0000000d";
+
+            if(listFavorite != null && listFavorite.includes(countryDetail.name.common)){
+                colorStar = "#FFD700";
+            }else{
+                colorStar = "#0000000d";
+            }
             
             if(countryDetail.borders){
                 listCountry = countryDetail.borders.toString()+"."
@@ -41,10 +68,12 @@ class Modal extends React.Component{
                 <div className={styles.modal}>
                     <div className={styles.modalContent}>
                         <span className={styles.close} onClick={this.props.closeModal}>&times;</span>
+
                         <p className={styles.title}>{countryDetail.name.common} 
                             <FontAwesomeIcon icon={faStar} color={colorStar} 
                             onClick={this.addToFavorite}/>
                         </p>
+
                         <p className={styles.textField}><b>Region:</b> {countryDetail.region}</p>
                         <p className={styles.textField}><b>Population:</b> {population}M</p>
                         <p className={styles.textField}><b>Capital:</b> {countryDetail.capital}</p>
